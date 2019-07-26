470a1624b67aca8b81ed936744c6b60c4121af3b
Kyler Barber <kylerbarber820@yahoo.com>
2019-07-26 23:22:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require 'pry'
+def prime?(number)
+  i = 2
+  if number == 2
+    true
+  elsif number > 2
+    piece = (i...number).to_a
+    piece.none? do |num|
+      number % num == 0
+    end
+  else
+    false
+  end
+end
